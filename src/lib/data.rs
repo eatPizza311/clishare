c9d53bec4a6c6cd7c5154c091b6bbf243124524c
ChihYing <spacetime0311@gmail.com>
2024-09-19 17:52:19 +0800

    feat: Database wrapper

--- src/lib/data.rs
+++ src/lib/data.rs
@@ -2,7 +2,46 @@ use std::str::FromStr;
 
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::Sqlite;
 use uuid::Uuid;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+// Type alias for easier DBMS switch
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+// Generic structure that ensure type D has sqlx::Database implemented
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
+
+impl Database<Sqlite> {
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = sqlx::sqlite::SqlitePoolOptions::new()
+            .connect(connection_str)
+            .await;
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(e) => {
+                eprintln!("{}\n", e);
+                eprintln!(
+                    "If the database has not yet been created, run: \n    $ sqlx database setup\n"
+                );
+                panic!("database connection error")
+            }
+        }
+    }
+
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
+    }
+}
 
 #[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
 pub struct DbId(Uuid);
