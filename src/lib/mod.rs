bd680766c7df8b7681ea0e6c21df1fcf46a1d447
ChihYing <spacetime0311@gmail.com>
2024-09-19 23:06:07 +0800

    feat: Data model

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,2 +1,7 @@
 pub mod data;
 pub mod domain;
+
+// Reexport some frequently used type to crate root
+pub use domain::clip::field::ShortCode;
+pub use domain::clip::ClipError;
+pub use domain::time::Time;
