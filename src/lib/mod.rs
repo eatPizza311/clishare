5c39946e8e5d51c55d5ec5dc9343513f2f672778
ChihYing <spacetime0311@gmail.com>
2024-09-21 23:04:56 +0800

    feat: Rocket server builder

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -10,3 +10,23 @@ pub use domain::clip::ClipError;
 pub use domain::time::Time;
 pub use domain::Clip;
 pub use service::ServiceError;
+
+use data::AppDatabase;
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
+use web::renderer::Renderer;
+
+// Build the Rocket server
+pub struct RocketConfig {
+    pub renderer: Renderer<'static>,
+    pub database: AppDatabase,
+}
+
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+    rocket::build()
+        .manage::<AppDatabase>(config.database)
+        .manage::<Renderer>(config.renderer)
+        .mount("/", web::http::routes())
+        .mount("/static", FileServer::from("static"))
+        .register("/", web::http::catcher::catchers())
+}
